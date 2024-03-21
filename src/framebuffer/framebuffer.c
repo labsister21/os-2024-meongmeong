4f80e79350fa7f1c9d27a08fae75395f56f6e817
amalia <amaliaputriii2104@gmail.com>
2024-03-21 20:03:38 +0700

    feat: include

--- src/framebuffer/framebuffer.c
+++ src/framebuffer/framebuffer.c
@@ -1,18 +1,21 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include "../header/text/framebuffer.h"
+#include "../header/driver/framebuffer.h"
 #include "../header/stdlib/string.h"
 #include "../header/cpu/portio.h"
 
-void framebuffer_set_cursor(uint8_t r, uint8_t c) {
+void framebuffer_set_cursor(uint8_t r, uint8_t c)
+{
     // TODO : Implement
 }
 
-void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
+void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
+{
     // TODO : Implement
 }
 
-void framebuffer_clear(void) {
+void framebuffer_clear(void)
+{
     // TODO : Implement
 }
