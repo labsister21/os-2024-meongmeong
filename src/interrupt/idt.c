6bfafa6e87e97755f8a25eb890185ba8290d6f8a
Julian <julianchandrasutadi@gmail.com>
2024-03-21 22:03:57 +0700

    feat: interrupt descriptor table definition

--- src/interrupt/idt.c
+++ src/interrupt/idt.c
@@ -1,2 +1,8 @@
 #include "../header/cpu/idt.h"
 
+struct InterruptDescriptorTable interrupt_descriptor_table;
+
+struct IDTR _idt_idtr = {
+    .size = sizeof(interrupt_descriptor_table) - 1,
+    .address = &interrupt_descriptor_table,
+}
