1c64618409e67b69e5d21cb2eeac3643f788ce67
angiekierra <13522048@std.stei.itb.ac.id>
2024-04-01 19:44:15 +0700

    feat : createfat, read_cluster, write_cluster

--- src/filesystem/fat32.c
+++ src/filesystem/fat32.c
@@ -3,7 +3,10 @@
 #include <stddef.h>
 #include "../header/stdlib/string.h"
 #include "../header/filesystem/fat32.h"
+#include "../header/driver/disk.h"
 
+
+struct FAT32DriverState fat32_driver_state;
 const uint8_t fs_signature[BLOCK_SIZE] = {
     'C', 'o', 'u', 'r', 's', 'e', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',  ' ',
     'D', 'e', 's', 'i', 'g', 'n', 'e', 'd', ' ', 'b', 'y', ' ', ' ', ' ', ' ',  ' ',
@@ -13,3 +16,53 @@ const uint8_t fs_signature[BLOCK_SIZE] = {
     [BLOCK_SIZE-2] = 'O',
     [BLOCK_SIZE-1] = 'k',
 };
+
+
+uint32_t cluster_to_lba(uint32_t cluster){
+}
+
+
+/**
+ * Initialize DirectoryTable value with
+ * - Entry-0: DirectoryEntry about itself
+ * - Entry-1: Parent DirectoryEntry
+ *
+ * @param dir_table          Pointer to directory table
+ * @param name               8-byte char for directory name
+ * @param parent_dir_cluster Parent directory cluster number
+ */
+void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster){}
+
+/**
+ * Create new FAT32 file system. Will write fs_signature into boot sector and
+ * proper FileAllocationTable (contain CLUSTER_0_VALUE, CLUSTER_1_VALUE,
+ * and initialized root directory) into cluster number 1
+ */
+
+void create_fat32(void){
+    write_blocks(fs_signature,BOOT_SECTOR,1);
+    fat32_driver_state.fat_table.cluster_map[0] = CLUSTER_0_VALUE;
+    fat32_driver_state.fat_table.cluster_map[1] = CLUSTER_1_VALUE;
+    fat32_driver_state.fat_table.cluster_map[2] = FAT32_FAT_END_OF_FILE;
+
+    for (int i=3 ; i < CLUSTER_MAP_SIZE;i++){
+        fat32_driver_state.fat_table.cluster_map[i] = FAT32_FAT_EMPTY_ENTRY;
+    }
+    write_clusters(fat32_driver_state.fat_table.cluster_map,FAT_CLUSTER_NUMBER,1);
+    struct FAT32DirectoryTable root_folder;
+    init_directory_table(&root_folder,"rootfolder",ROOT_CLUSTER_NUMBER);
+    write_clusters(&root_folder,ROOT_CLUSTER_NUMBER,1);
+
+}
+
+void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_count){
+    write_blocks(ptr,cluster_to_lba(cluster_number),cluster_count * CLUSTER_BLOCK_COUNT);
+}
+
+void read_clusters(void *ptr, uint32_t cluster_number, uint8_t cluster_count){
+    read_blocks(ptr,cluster_to_lba(cluster_number),cluster_count * CLUSTER_BLOCK_COUNT);
+}
+
+
+
+
